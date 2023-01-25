76e6e16e0877575711c1945edb3ac12074efe83c
Damian Traczuk <damian.traczuk@linuxpolska.pl>
2023-01-25 19:42:48 +0100

    Modyfikacja

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from time import strftime, sleep
 import socket
+
+import pandas as pd
 from urllib3.connection import HTTPSConnection
 import requests
 import xmltodict
@@ -20,7 +22,8 @@ if __name__ == '__main__':
     url='https://192.168.231.160:8089'
     login='admin'
     password='qaz123456'
-    earliest='-3d@'
+    earliest='-5m@'
+    number_analyzed_events=100
     data = {
         'search': 'search index=_audit action=search "search"=* earliest='+earliest+' latest=now | fields search'
     }
@@ -28,7 +31,7 @@ if __name__ == '__main__':
     job_tree = E.ElementTree(E.fromstring(response.content.decode('utf-8'))).getroot()
     sid = job_tree.find('./sid').text
     searches_from_month = []
-    fields_from_month = {}
+    test_df=pd.DataFrame(columns=['index','source','sourcetype'])
     while (True):
         response = requests.get(url+'/services/search/jobs/' + sid, data={'output_mode': 'json'}, verify=False, auth=(login,password))
         if (response.json()['entry'][0]['content']['dispatchState'] == 'DONE'):
@@ -40,18 +43,20 @@ if __name__ == '__main__':
             searches_from_month.append(result.find("./field[@k='search']/value/text").text)
     for search in searches_from_month:
         data = {
-            "search": search.strip('\'')
+            "search": search.strip('\''),
+            "auto_finalize_ec":number_analyzed_events,
+            "rf":"*"
         }
         response = requests.post(url+'/services/search/jobs', data=data, verify=False,auth=(login,password))
         if (response.status_code >= 200 and response.status_code < 300):
             search_sid = E.ElementTree(E.fromstring(response.content.decode('utf-8'))).getroot().find('./sid').text
             command = {
-                'action': 'finalize'
+                'action': 'cancel',
             }
             sleep(2)
             search_results = response.content.decode('utf-8')
-            requests.post(url+'/services/search/jobs/' + search_sid + "/control", data=command,verify=False, auth=(login, password))
             response = requests.get(url+'/services/search/jobs/' + search_sid + '/results_preview', verify=False,auth=(login,password))
+            #requests.post(url+'/services/search/jobs/' + search_sid + "/control", data=command,verify=False, auth=(login, password))
             used_fields_months = {}
             if (response.content.decode('utf-8') != ''):
                 job_tree = E.ElementTree(E.fromstring(response.content.decode('utf-8'))).getroot()
@@ -59,48 +64,38 @@ if __name__ == '__main__':
                     t_index = result.find("./field[@k='index']")
                     if t_index is not None:
                         t_index = t_index.find('./value/text')
-                        print(t_index.text)
+                        filter_index = test_df['index'] == t_index.text
                     t_source = result.find("./field[@k='source']")
                     if t_source is not None:
                         t_source = t_source.find('./value/text')
+                        filter_source = test_df['source'] == t_source.text
                     t_sourcetype = result.find("./field[@k='sourcetype']")
                     if t_sourcetype is not None:
                         t_sourcetype = t_sourcetype.find('./value/text')
+                        filter_sourcetype = test_df['sourcetype'] == t_sourcetype.text
+                    if t_index is not None and t_sourcetype is not None and t_source is not None:
+                        if not ((filter_index) & (filter_sourcetype) & (filter_source)).any():
+                            pomoc = {'index': t_index.text, 'source': t_source.text, 'sourcetype': t_sourcetype.text}
+                            test_df2 = pd.DataFrame(data=pomoc, index=[0])
+                            test_df = pd.concat([test_df,test_df2], ignore_index=True)
+                            test_df=test_df.fillna(0)
                     for field in result:
                         if (field.tag == 'field'):
                             if t_index is not None and t_sourcetype is not None and t_source is not None:
                                 used_fields_months[t_index.text] = used_fields_months.get(t_index.text, {})
+                                used_fields_months[t_index.text][t_source.text] = used_fields_months[t_index.text].get(t_source.text,{})
+                                used_fields_months[t_index.text][t_source.text][t_sourcetype.text] = used_fields_months[t_index.text][
+                                    t_source.text].get(t_sourcetype.text, {})
+
+                                if not field.attrib['k'] in test_df.columns:
+                                    test_df[field.attrib['k']] = 0
                                 if (used_fields_months[t_index.text].get(field.attrib['k'], False) == False and
                                         field.attrib['k'] != 'source' and field.attrib['k'] != 'sourcetype' and
                                         field.attrib['k'] != 'index'):
-                                    fields_from_month[t_index.text] = fields_from_month.get(t_index.text, {})
-                                    fields_from_month[t_index.text][t_source.text] = fields_from_month.get(t_index.text,
-                                                                                                           {}).get(
-                                        t_source.text, {})
-                                    fields_from_month[t_index.text][t_source.text][
-                                        t_sourcetype.text] = fields_from_month.get(t_index.text, {}).get(t_source.text,
-                                                                                                         {}).get(
-                                        t_sourcetype.text, {})
-                                    fields_from_month[t_index.text][t_source.text][t_sourcetype.text][
-                                        field.attrib['k']] = fields_from_month.get(t_index.text, {}).get(t_source.text,
-                                                                                                         {}).get(
-                                        t_sourcetype.text, {}).get(field.attrib['k'], 0) + 1
+                                    test_df[field.attrib['k']]=test_df[field.attrib['k']].mask(filter_sourcetype & filter_source & filter_index,test_df[field.attrib['k']]+1)
                                     used_fields_months[t_index.text][field.attrib['k']] = used_fields_months.get(
                                         t_index.text).get(field.attrib['k'], True)
 
-    if (fields_from_month != {}):
-        data = []
-        for index in fields_from_month:
-            pdata = {'index': index}
-            for source in fields_from_month[index]:
-                pdata['source'] = source
-                for sourcetype in fields_from_month[index][source]:
-                    pdata['sourcetype'] = sourcetype
-                    for pair in fields_from_month[index][source][sourcetype]:
-                        pdata[pair] = fields_from_month[index][source][sourcetype][pair]
-            data.append(pdata)
-        dfm = pandas.DataFrame(data=data)
-        dfm = dfm.fillna(0)
-        dfm.to_csv('report_time.csv', index=False)
-        print(dfm)
+        test_df.to_csv('report_time.csv', index=False)
+        print(test_df)
 
